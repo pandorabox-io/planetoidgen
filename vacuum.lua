edd93a194cac4048da25bf97965abe440d256ce4
NatureFreshMilk <naturefreshmilk@github.com>
2019-06-25 14:50:11 +0200

    remove no_abm hook

--- vacuum.lua
+++ vacuum.lua
@@ -22,18 +22,4 @@ if has_vacuum_mod then
     return old_is_pos_in_space(pos)
   end
 
-  local old_no_vacuum_abm = vacuum.no_vacuum_abm
-  vacuum.no_vacuum_abm = function(pos)
-    for _, planet in ipairs(planetoidgen.planets) do
-      local distance = vector.distance(pos, planet.pos)
-
-      if distance < planet.radius+10 and distance > planet.radius-10 then
-        -- no air/vacuum interaction at planet boundary
-        return planet.airshell
-      end
-    end
-
-    return old_no_vacuum_abm(pos)
-  end
-
 end
