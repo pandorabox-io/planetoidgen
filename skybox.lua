2a9f9d89d38feaf5246726d24a6b0f8d521d1cce
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-04-28 13:48:29 +0200

    revert skybox to simple 1-part instead of 5-part gradient

--- skybox.lua
+++ skybox.lua
@@ -18,40 +18,30 @@ skybox.register({
 	}
 })
 
-for i=1,5 do
- local col = "^[colorize:#7476F2:" .. i * 20
+local col = "^[colorize:#7476F2:" .. 20
 
-	skybox.register({
-		-- https://github.com/Ezhh/other_worlds/blob/master/skybox.lua
-		name = "deepspace_" .. i,
-		always_day = true,
-		fly = true,
-		match = function(_, pos)
-			local planet = planetoidgen.get_planet_at_pos(pos)
-			if planet then
-				local distance = vector.distance(pos, planet.pos)
+skybox.register({
+	-- https://github.com/Ezhh/other_worlds/blob/master/skybox.lua
+	name = "deepspace_planet",
+	always_day = true,
+	fly = true,
+	match = function(_, pos)
+		local planet = planetoidgen.get_planet_at_pos(pos)
+		if planet then
+			local distance = vector.distance(pos, planet.pos)
 
-				if distance < planet.radius then
-					local factor = distance / planet.radius
-					if factor < 0.5 and i == 5 or
-						factor < 0.6 and i == 4 or
-						factor < 0.7 and i == 3 or
-						factor < 0.8 and i == 2 or
-						factor < 1 and i == 1
-					then
-						return true
-					end
-				end
+			if distance < planet.radius then
+				return true
 			end
-		end,
-		priority = i,
-		textures = {
-			"sky_pos_z.png" .. col,
-			"sky_neg_z.png^[transformR180" .. col,
-			"sky_neg_y.png^[transformR270" .. col,
-			"sky_pos_y.png^[transformR270" .. col,
-			"sky_pos_x.png^[transformR270" .. col,
-			"sky_neg_x.png^[transformR90" .. col
-		}
-	})
-end
+		end
+	end,
+	priority = 1,
+	textures = {
+		"sky_pos_z.png" .. col,
+		"sky_neg_z.png^[transformR180" .. col,
+		"sky_neg_y.png^[transformR270" .. col,
+		"sky_pos_y.png^[transformR270" .. col,
+		"sky_pos_x.png^[transformR270" .. col,
+		"sky_neg_x.png^[transformR90" .. col
+	}
+})
