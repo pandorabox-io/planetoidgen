d66b618fa8fc5fc265d6f5abcc50ddd0fbfaf939
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-09-21 12:12:21 +0200

    custom planets test

---
+++ dev/custom_planets/init.lua
@@ -0,0 +1,7 @@
+planetoidgen.register_planet({
+  pos = { x=2500, y=7500, z=2500 },
+  name = "my-planet",
+  type = "class-m",
+  radius = 300,
+  airshell = true
+})
--- dev/start_minetest.sh
+++ dev/start_minetest.sh
@@ -9,6 +9,8 @@ docker run --rm -it \
 	-u root:root \
 	-v $(pwd)/minetest.conf:/etc/minetest/minetest.conf \
 	-v $(pwd)/worldmods:/root/.minetest/worlds/world/worldmods \
+	-v $(pwd)/custom_planets:/root/.minetest/worlds/world/worldmods/custom_planets \
+	-v $(pwd)/../:/root/.minetest/worlds/world/worldmods/planetoidgen \
 	-v $(pwd)/data:/root/.minetest/worlds/world \
 	--network host \
 	registry.gitlab.com/minetest/minetest/server:5.3.0
